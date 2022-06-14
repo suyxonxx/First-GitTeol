bfbbfd19ecbe8a5dccd5c9e5b7b93c5881553d9a
suyxonxx <suyxonxx@gmail.com>
2022-06-14 22:55:58 +0900

    220614

--- On220613_02/src/main/webapp/script/product.js
+++ On220613_02/src/main/webapp/script/product.js
@@ -1,3 +1,18 @@
-/**
- * 
- */
+function productCheck() {
+	if(document.frm.name.value.length == 0) {
+  	alert("상품명을 입력해주세요.");
+    frm.name.focus();
+    return false;
+  }
+	if(document.frm.price.value.length == 0) {
+  	alert("가격을 입력해주세요.");
+    frm.price.focus();
+    return false;
+  }
+	if(document.frm.description.value.length == 0) {
+  	alert("설명을 입력해주세요.");
+    frm.description.focus();
+    return false;
+  }
+  return true;
+}
--- On220614_02/src/main/webapp/script/movie.js
+++ On220614_02/src/main/webapp/script/movie.js
@@ -2,27 +2,27 @@ function movieCheck() {
 	if(document.frm.title.value.length == 0) {
   	alert("제목을 입력해주세요.");
     frm.title.focus();
-    return 0;
+    return false;
   }
 	if(document.frm.price.value.length == 0) {
   	alert("가격을 입력해주세요.");
     frm.price.focus();
-    return 0;
+    return false;
   }
 	if(document.frm.director.value.length == 0) {
   	alert("감독을 입력해주세요.");
     frm.director.focus();
-    return 0;
+    return false;
   }
 	if(document.frm.actor.value.length == 0) {
   	alert("배우를 입력해주세요.");
     frm.actor.focus();
-    return 0;
+    return false;
   }
 	if(document.frm.synopsis.value.length == 0) {
   	alert("시놉시스를 입력해주세요.");
     frm.synopsis.focus();
-    return 0;
+    return false;
   }
-  return 1;
+  return true;
 }
