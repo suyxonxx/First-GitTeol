153096cce36dd05d79809eedd47c4c65a8e4b537
suyxonxx <suyxonxx@gmail.com>
2022-07-04 17:49:02 +0900

    220704

--- On220620_02/src/main/java/com/springbook/view/user/LoginController.java
+++ On220620_02/src/main/java/com/springbook/view/user/LoginController.java
@@ -1,36 +1,17 @@
 package com.springbook.view.user;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.springbook.biz.user.UserVO;
 import com.springbook.biz.user.impl.UserDAO;
 
-public class LoginController implements Controller {
-	@Override
-	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) {
-		System.out.println("로그인 처리");
-		
-		String id = request.getParameter("id");
-		String password = request.getParameter("password");
-		
-		UserVO vo = new UserVO();
-		vo.setId(id);
-		vo.setPassword(password);
-		
-		UserDAO userDAO = new UserDAO();
-		UserVO user = userDAO.getUser(vo);
-		
-//		3. 화면 네비게이션
-		ModelAndView mav = new ModelAndView();
-		if(user != null) {
-			mav.setViewName("getBoardList.do");
-		} else {
-			mav.setViewName("login.jsp");
-		}
-		return mav;
+@Controller
+public class LoginController {
+	
+	@RequestMapping("/login.do")
+	public String login(UserVO vo, UserDAO userDAO) {
+		if(userDAO.getUser(vo) != null) return "getBoardList.do";
+		else return "login.jsp";
 	}
 }
